74bd9d8d4a96d3f7b4999ba345bdcc082a7d8cdc
Kip Macy <kmacy@freebsd.org>
2011-12-23 17:44:10 +0100

    commit re-factor of getauxv in to host code

--- source/Host/freebsd/Host.cpp
+++ source/Host/freebsd/Host.cpp
@@ -16,6 +16,11 @@
 #include <sys/utsname.h>
 #include <sys/sysctl.h>
 
+#include <sys/ptrace.h>
+#include <sys/exec.h>
+#include <machine/elf.h>
+
+
 // C++ Includes
 // Other libraries and framework includes
 // Project includes
@@ -27,7 +32,10 @@
 #include "lldb/Core/StreamString.h"
 #include "lldb/Target/Process.h"
 
+#include "lldb/Core/DataBufferHeap.h"
+#include "lldb/Core/DataExtractor.h"
 #include "llvm/Support/Host.h"
+
 
 extern "C" {
     char **environ;
@@ -276,4 +284,47 @@ Host::GetProcessInfo (lldb::pid_t pid, ProcessInstanceInfo &process_info)
     }
     process_info.Clear();
     return false;
+}
+
+lldb::DataBufferSP
+Host::GetAuxvData(lldb_private::Process *process)
+{
+   int mib[2] = { CTL_KERN, KERN_PS_STRINGS };
+   void *ps_strings_addr, *auxv_addr;
+   size_t ps_strings_size = sizeof(void *);
+   Elf_Auxinfo aux_info[AT_COUNT];
+   struct ps_strings ps_strings;
+   struct ptrace_io_desc pid;
+   DataBufferSP buf_sp;
+   std::auto_ptr<DataBufferHeap> buf_ap(new DataBufferHeap(1024, 0));
+
+   if (::sysctl(mib, 2, &ps_strings_addr, &ps_strings_size, NULL, 0) == 0) {
+	   pid.piod_op = PIOD_READ_D;
+	   pid.piod_addr = &ps_strings;
+	   pid.piod_offs = ps_strings_addr;
+	   pid.piod_len = sizeof(ps_strings);
+	   if (::ptrace(PT_IO, process->GetID(), (caddr_t)&pid, NULL)) {
+		   perror("failed to fetch ps_strings");
+		   buf_ap.release();
+		   goto done;
+	   }
+		   
+	   auxv_addr = ps_strings.ps_envstr + ps_strings.ps_nenvstr + 1;
+	   
+	   pid.piod_addr = aux_info;
+	   pid.piod_offs = auxv_addr;
+	   pid.piod_len = sizeof(aux_info);
+	   if (::ptrace(PT_IO, process->GetID(), (caddr_t)&pid, NULL)) {
+		   perror("failed to fetch aux_info");
+		   buf_ap.release();
+		   goto done;
+	   }
+	   memcpy(buf_ap->GetBytes(), aux_info, pid.piod_len);
+	   buf_sp.reset(buf_ap.release());
+   } else {
+	   perror("sysctl failed on ps_strings");
+   }
+
+   done:
+   return buf_sp;
 }
